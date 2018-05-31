adaf2997a1679c58b57aebae91e987d92bdd9208
renee.dudley <renee.dudley@malauzai.com>
2018-05-31 18:01:16 -0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {
+     posts: [
+         {id: 0, author: 'Ted',  text: 'today I thought that bacon was so great', time: 'Mon May 28 2018 16:12:25 GMT-0500 (CDT)'},
+         {id: 1, author: 'Martha',  text: 'having a great time in spain!', time: 'Wed May 30 2018 08:12:25 GMT-0500 (CDT)'},
+         {id: 3, author: 'Mark',  text: 'Just got back from my trip it was great', time: 'Wed May 30 2018 11:59:25 GMT-0500 (CDT)'},
+     ],
+     postInput: '',
+     userNameInput: '',
+  };
+  renderPosts(){
+      return this.state.posts.map((post, index) => {
+          const description = `${post.author} - ${post.time}`;
+          return (
+              <div>
+                  <p>{post.text}</p>
+                  <p style={{fontSize: '12px'}}>{description}</p>
+              </div>
+          )
+      }).reverse()
+  }
+
+ handlePostInputChange(e){
+     this.setState({postInput: e.target.value})
+ }
+ handleUserNameInputChange(e){
+     this.setState({userNameInput: e.target.value})
+ }
+ handleSubmitPost(){
+     const newPost = {
+         author: this.state.userNameInput,
+         text: this.state.postInput,
+         id: this.state.posts.length + 1,
+         time: new Date()
+     };
+     const posts = this.state.posts.map(post => post);
+     posts.push(newPost);
+     this.setState({posts, postInput: '', userNameInput: '',})
+ }
+
+ renderSubmit(){
+    if (this.state.postInput && this.state.userNameInput) {
+        return <button onClick={() => this.handleSubmitPost()}>Submit post</button>
+    }
+ }
+
+ render() {
+   return (
+     <div className="App">
+         <div style={{
+             display: 'flex',
+             flexDirection: 'column',
+             justifyContent: 'center',
+             alignItems: 'center',
+         }}>
+            <input value={this.state.postInput} onChange={(e) => this.handlePostInputChange(e)} placeholder="write something..."/>
+            <input value={this.state.userNameInput} onChange={(e) => this.handleUserNameInputChange(e)} placeholder="username"/>
+            {this.renderSubmit()}
+         </div>
+         {this.renderPosts()}
+     </div>
+   );
+ }
+}
+
+
+export default App;
